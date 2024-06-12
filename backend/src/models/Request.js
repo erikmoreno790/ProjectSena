6689dae33f691bef66762c5b8af01eed990814c6
Erik Moreno <erikmoreno790@gmail.com>
2024-06-12 12:15:39 -0500

    Añadimos el modelo Request para manejar las solicitudes de los servicios

---
+++ backend/src/models/Request.js
@@ -0,0 +1,30 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const RequestSchema = new Schema({
+  client: {
+    type: Schema.Types.ObjectId,
+    ref: 'User',
+    required: true
+  },
+  service: {
+    type: Schema.Types.ObjectId,
+    ref: 'Service',
+    required: true
+  },
+  date: {
+    type: Date,
+    required: true
+  },
+  status: {
+    type: String,
+    enum: ['pending', 'accepted', 'declined', 'completed'],
+    default: 'pending'
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now
+  }
+});
+
+module.exports = mongoose.model('Request', RequestSchema);
