55364d382147f8438f4f25a49ae72e3c7d3a67e9
Erik Moreno <erikmoreno790@gmail.com>
2024-06-13 12:45:52 -0500

    Configuramos una clave secreta JWT fija para pruebas en Postman

--- backend/config.js
+++ backend/config.js
@@ -1,10 +1,9 @@
-const crypto = require('crypto');
 
 // Genera la clave secreta JWT
-const jwtSecret = crypto.randomBytes(64).toString('hex');
+const jwtSecret = 'JHDY87YDNCKJH3874NDSKJHF3489FHNSKJDH3R8FH4N38F7N4DKJH5F8NDJKH3R8';
 
 module.exports = {
     jwtSecret:  jwtSecret, 
-    mongoURI: 'mongodb://localhost:27017'
+    mongoURI: 'mongodb://localhost:27017/LocalFix'
   };
   
